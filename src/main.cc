34bd2888864f2c1a78eff9f1f4bf55edcdac638e
Brandon R <brandon@blrice.net>
2018-06-13 15:20:07 -0400

    Basic signal handling.

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,22 @@
+#include <stdlib.h>
 #include <time.h>
 
+#include <csignal>
 #include <iostream>
 
 #include "game.h"
 
+void HandleSignal(int signum) {
+  std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
+
+  // cleanup
+  
+  exit(signum);
+}
+
 int main() {
+  signal(SIGINT, HandleSignal);
+
   guemud::Game game;
 
   struct timespec loop_time, loop_remain;
@@ -18,7 +30,7 @@ int main() {
     nanosleep(&loop_time, &loop_remain); // yield thread to OS
   }
 
-  std::cout << "Exited!" << std::endl;
+  std::cout << "Exited." << std::endl;
 
   return 0;
 }
